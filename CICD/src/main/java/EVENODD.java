4106e61a5752327fb02251861354c849b6da3023
SHIRI <Shiri957313@gmail.com>
2026-01-21 12:12:43 +0530

    enter a number

---
+++ CICD/src/main/java/EVENODD.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EVENODD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer to check: ");
+        int number = scanner.nextInt();
+        
+        if (number % 2 == 0) {
+            System.out.println("The given number " + number + " is Even");
+        } else {
+            System.out.println("The given number " + number + " is Odd");
+        }
+        
+        scanner.close(); 
+    }
+}
+
